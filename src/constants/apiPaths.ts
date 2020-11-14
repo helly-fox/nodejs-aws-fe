72127e6f4e23ea169156e1ef928823129c6b1ecd
Alena Korsunova <alenha.korsunova@nike.com>
2020-11-15 00:36:55 +0300

    Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://nkmnmeycd8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://8q4eq2nsm0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://nkmnmeycd8.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
